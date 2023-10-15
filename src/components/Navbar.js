07e72396d384d523efd5a082a43435a9cd04552f
konradik <karinakonradi1@gmail.com>
2023-10-15 15:48:23 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="container">
+            <h1 className="main-text">
+                IP Address Tracker
+            </h1>
+            <div className="input-container">
+                <input type="text" className="input-style"/>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Navbar;
